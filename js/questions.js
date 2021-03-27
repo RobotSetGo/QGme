e33b8223cc2ac841220ff2cdc22fdcfe25b81f0b
Apostolos Siokas <apostolossiokas@gmail.com>
2021-03-27 10:21:42 +0200

    Add project to repo

---
+++ js/questions.js
@@ -0,0 +1,16 @@
+var questions = [
+    {
+        "question": "Ο Bruce Wane είναι ο Superman",
+        "answer": false,
+        "wiki": "Ο Bruce Wane είναι ο Batman. Ο Superman ονομάζεται Clark Kent!"
+    },
+    {
+        "question": "Ο Spiderman είναι αράχνη",
+        "answer": true
+    },
+    {
+        "question": "To pikatchu πετάει φωτιές",
+        "answer": false,
+        "wiki": "To pikatchu πετάει ρεύμα!"
+    },
+];
